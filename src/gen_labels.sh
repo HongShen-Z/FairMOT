f0b0b3429f6b9387228388af638614033d5a3509
ZHS <com.zhs@gmail.com>
2022-07-10 22:39:06 +0800

    gen mot17 labels

--- src/gen_labels.sh
+++ src/gen_labels.sh
@@ -2,13 +2,13 @@
 #BSUB -J gen_labels
 #BSUB -o %J.out
 #BSUB -e %J.err
-#BSUB -q gpu_v100
-#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+#BSUB -q normal
 
 module load anaconda3
 source activate
 conda deactivate
 conda activate fairmot
 
-python gen_labels_16.py
+python gen_labels_15.py
+python gen_labels_20.py
 #~/.cache/torch/checkpoints/yolov5s.pt
