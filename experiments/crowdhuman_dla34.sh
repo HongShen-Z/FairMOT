8063246dd5c60897199aaea674f439199f5f67bb
ZHS <com.zhs@gmail.com>
2022-07-12 17:01:01 +0800

    training test

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#BSUB -J train_ch
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -q gpu_v100
+#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+
+module load anaconda3
+source activate
+conda deactivate
+conda activate fairmot
 cd src
-python train.py mot --exp_id crowdhuman_dla34 --gpus 0,1 --batch_size 8 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 60 --lr_step '50' --data_cfg '../src/lib/cfg/crowdhuman.json'
+python train.py mot --exp_id crowdhuman_dla34 --gpus $CUDA_VISIBLE_DEVICES --batch_size 8 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 60 --lr_step '50' --data_cfg '../src/lib/cfg/crowdhuman.json'
 cd ..
