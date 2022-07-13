784287fffba1b4d04bca6b32b4e879c7cc42d2e0
ZHS <com.zhs@gmail.com>
2022-07-13 14:07:24 +0800

    training test

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -10,7 +10,7 @@ source activate
 conda deactivate
 conda activate fairmot
 cd src
-python train.py mot --exp_id crowdhuman_dla34 --gpus $CUDA_VISIBLE_DEVICES \
+python train.py mot --exp_id ch_dla34 --gpus $CUDA_VISIBLE_DEVICES \
 --batch_size 64 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 60 \
 --lr_step '50' --lr 5e-4 --data_cfg '../src/lib/cfg/crowdhuman.json'
 cd ..
