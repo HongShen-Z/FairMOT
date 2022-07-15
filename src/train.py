4f319f83c76f108af2b23e3649144ba31d623346
ZHS <com.zhs@gmail.com>
2022-07-15 10:19:55 +0800

    training test

--- src/train.py
+++ src/train.py
@@ -58,6 +58,7 @@ def main(opt):
     print('Starting training...')
     Trainer = train_factory[opt.task]
     trainer = Trainer(opt, model, optimizer)
+    print(opt.gpus, opt.device)
     trainer.set_device(opt.gpus, opt.chunk_sizes, opt.device)
 
     if opt.load_model != '':
