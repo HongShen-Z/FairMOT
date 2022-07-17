f2eaf4824b62b218efde0d6afc7076206b47d267
ZHS <com.zhs@gmail.com>
2022-07-17 20:44:25 +0800

    tracking test

--- src/track.sh
+++ src/track.sh
@@ -9,7 +9,7 @@ module load anaconda3
 source activate
 conda deactivate
 conda activate fair
-python track.py mot --val_mot17 True --load_model ../exp/mot/mix_ft_ch_dla34/model_last.pth --conf_thres 0.4 --gpus $CUDA_VISIBLE_DEVICES
+python track.py mot --test_mot17 True --load_model ../exp/mot/mix_ft_ch_dla34/model_last.pth --conf_thres 0.4 --gpus $CUDA_VISIBLE_DEVICES
 #python track.py mot --exp_id MOT15val_dla34 --load_model ../models/fairmot_dla34.pth \
 #--conf_thres 0.6 --gpus $CUDA_VISIBLE_DEVICES
 
