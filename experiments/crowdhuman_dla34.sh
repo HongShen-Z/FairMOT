eb4480da99b5e1085a71c3ce88bf76e6ba353ad4
ZHS <com.zhs@gmail.com>
2022-07-21 15:37:21 +0800

    change varifocal loss for training heatmap

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -10,7 +10,7 @@ source activate
 conda deactivate
 conda activate fair
 cd src
-python train.py mot --exp_id ch_dla34_varifocal --gpus $CUDA_VISIBLE_DEVICES \
+python train.py mot --exp_id ch_dla34_varifocal_3 --gpus $CUDA_VISIBLE_DEVICES \
 --batch_size 32 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 60 \
 --lr_step '50' --lr 2e-4 --data_cfg '../src/lib/cfg/crowdhuman.json' --hm_weight 10.0
 cd ..
