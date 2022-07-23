a44acc71333690ccef6b3a7b7d458730240fc0ae
ZHS <com.zhs@gmail.com>
2022-07-23 17:09:45 +0800

    heatmap loss evals

--- src/track.sh
+++ src/track.sh
@@ -9,8 +9,8 @@ module load anaconda3
 source activate
 conda deactivate
 conda activate fair
-python track.py mot --val_mot17 True --load_model ../exp/mot/ch_dla34_varifocal_4/models/model_55.pth --conf_thres 0.4 \
---gpus $CUDA_VISIBLE_DEVICES --exp_id MOT17_mod55
+python track.py mot --val_mot17 True --load_model ../exp/mot/ch_dla34_varifocal_4/models/model_50.pth --conf_thres 0.4 \
+--gpus $CUDA_VISIBLE_DEVICES --exp_id MOT17_mod50
 #python track.py mot --exp_id MOT15val_dla34 --load_model ../models/fairmot_dla34.pth \
 #--conf_thres 0.6 --gpus $CUDA_VISIBLE_DEVICES
 
