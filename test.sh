9766284414c2bf9b8faf9caecb5da837f5f31814
ZHS <com.zhs@gmail.com>
2022-08-07 17:59:07 +0800

    wh reg change to giouloss

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#BSUB -J test
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -q gpu_v100
+#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+
+module load anaconda3
+source activate
+conda deactivate
+conda activate fair
+python test.py
