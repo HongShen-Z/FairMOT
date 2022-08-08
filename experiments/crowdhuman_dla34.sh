508fa9c4b7ff8e17d791eed3b2cf0851c29d5d12
ZHS <com.zhs@gmail.com>
2022-08-08 20:38:30 +0800

    wh reg change to giouloss

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -12,5 +12,5 @@ conda activate fair
 cd src
 python train.py mot --exp_id ch_dla34_wh --gpus $CUDA_VISIBLE_DEVICES \
 --batch_size 16 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 80 \
---lr_step '20,50,70' --lr 0.015 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
+--lr_step '2,50,70' --lr 0.015 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
 cd ..
