30e5bfb3b96ab673395010491e9f36dda68be7b0
ZHS <com.zhs@gmail.com>
2022-08-10 15:56:11 +0800

    wh reg change to giouloss

--- src/track.sh
+++ src/track.sh
@@ -9,7 +9,7 @@ module load anaconda3
 source activate
 conda deactivate
 conda activate fair
-python track.py mot --val_mot20 True --load_model ../exp/mot/ch_dla34_wh3/models/model_74.pth --conf_thres 0.3 \
+python track.py mot --val_mot20 True --load_model ../exp/mot/ch_dla34_wh3/models/model_76.pth --conf_thres 0.3 \
 --gpus $CUDA_VISIBLE_DEVICES --exp_id MOT20_ch_wh3
 #python track.py mot --exp_id MOT15val_dla34 --load_model ../models/fairmot_dla34.pth \
 #--conf_thres 0.6 --gpus $CUDA_VISIBLE_DEVICES
