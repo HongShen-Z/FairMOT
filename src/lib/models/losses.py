cb3c327beb7dedd1ab48ace66efb91623f514c78
ZHS <com.zhs@gmail.com>
2022-08-11 21:28:47 +0800

    wh reg change to giouloss

--- src/lib/models/losses.py
+++ src/lib/models/losses.py
@@ -132,7 +132,7 @@ class GiouLoss(nn.Module):
 
     def forward(self, preds, weight, bbox, eps=1e-7, reduction='mean'):
         """
-       https://github.com/sfzhang15/ATSS/blob/master/atss_core/modeling/rpn/atss/loss.py#L36
+        DIOU Loss
         :param preds:[[x1,y1,x2,y2], [x1,y1,x2,y2],,,]
         :param bbox:[[x1,y1,x2,y2], [x1,y1,x2,y2],,,]
         :return: loss
@@ -145,30 +145,94 @@ class GiouLoss(nn.Module):
         print(preds)
         print('-' * 100)
         print(bbox)
-        x1 = torch.max(preds[:, 0], bbox[:, 0])
-        y1 = torch.max(preds[:, 1], bbox[:, 1])
-        x2 = torch.min(preds[:, 2], bbox[:, 2])
-        y2 = torch.min(preds[:, 3], bbox[:, 3])
+        ix1 = torch.max(preds[:, 0], bbox[:, 0])
+        iy1 = torch.max(preds[:, 1], bbox[:, 1])
+        ix2 = torch.min(preds[:, 2], bbox[:, 2])
+        iy2 = torch.min(preds[:, 3], bbox[:, 3])
 
-        w = (x2 - x1 + 1.0).clamp(0.)
-        h = (y2 - y1 + 1.0).clamp(0.)
+        iw = (ix2 - ix1 + 1.0).clamp(min=0.)
+        ih = (iy2 - iy1 + 1.0).clamp(min=0.)
 
-        inters = w * h
+        # overlaps
+        inters = iw * ih
 
+        # union
         uni = (preds[:, 2] - preds[:, 0] + 1.0) * (preds[:, 3] - preds[:, 1] + 1.0) + (
                     bbox[:, 2] - bbox[:, 0] + 1.0) * (
                       bbox[:, 3] - bbox[:, 1] + 1.0) - inters
 
-        ious = (inters / uni).clamp(min=eps)
-        loss = -ious.log()
+        # iou
+        iou = inters / (uni + eps)
+
+        # inter_diag
+        cxpreds = (preds[:, 2] + preds[:, 0]) / 2
+        cypreds = (preds[:, 3] + preds[:, 1]) / 2
+
+        cxbbox = (bbox[:, 2] + bbox[:, 0]) / 2
+        cybbox = (bbox[:, 3] + bbox[:, 1]) / 2
+
+        inter_diag = (cxbbox - cxpreds) ** 2 + (cybbox - cypreds) ** 2
+
+        # outer_diag
+        ox1 = torch.min(preds[:, 0], bbox[:, 0])
+        oy1 = torch.min(preds[:, 1], bbox[:, 1])
+        ox2 = torch.max(preds[:, 2], bbox[:, 2])
+        oy2 = torch.max(preds[:, 3], bbox[:, 3])
+
+        outer_diag = (ox1 - ox2) ** 2 + (oy1 - oy2) ** 2
+
+        diou = iou - inter_diag / outer_diag
+        diou = torch.clamp(diou, min=-1.0, max=1.0)
+
+        diou_loss = 1 - diou
 
         if reduction == 'mean':
-            loss = torch.mean(loss)
+            loss = torch.mean(diou_loss)
         elif reduction == 'sum':
-            loss = torch.sum(loss)
+            loss = torch.sum(diou_loss)
         else:
             raise NotImplementedError
         return loss
+
+    # def forward(self, preds, weight, bbox, eps=1e-7, reduction='mean'):
+    #     """
+    #     IOU Loss
+    #     :param preds:[[x1,y1,x2,y2], [x1,y1,x2,y2],,,]
+    #     :param bbox:[[x1,y1,x2,y2], [x1,y1,x2,y2],,,]
+    #     :return: loss
+    #     """
+    #     pos_mask = weight > 1e-3
+    #     # avg_factor = torch.sum(pos_mask).float().item() + 1e-4
+    #     preds = preds[pos_mask].view(-1, 4)
+    #     bbox = bbox[pos_mask].view(-1, 4)
+    #     print('#' * 100)
+    #     print(preds)
+    #     print('-' * 100)
+    #     print(bbox)
+    #     x1 = torch.max(preds[:, 0], bbox[:, 0])
+    #     y1 = torch.max(preds[:, 1], bbox[:, 1])
+    #     x2 = torch.min(preds[:, 2], bbox[:, 2])
+    #     y2 = torch.min(preds[:, 3], bbox[:, 3])
+    #
+    #     w = (x2 - x1 + 1.0).clamp(0.)
+    #     h = (y2 - y1 + 1.0).clamp(0.)
+    #
+    #     inters = w * h
+    #
+    #     uni = (preds[:, 2] - preds[:, 0] + 1.0) * (preds[:, 3] - preds[:, 1] + 1.0) + (
+    #                 bbox[:, 2] - bbox[:, 0] + 1.0) * (
+    #                   bbox[:, 3] - bbox[:, 1] + 1.0) - inters
+    #
+    #     ious = (inters / uni).clamp(min=eps)
+    #     loss = -ious.log()
+    #
+    #     if reduction == 'mean':
+    #         loss = torch.mean(loss)
+    #     elif reduction == 'sum':
+    #         loss = torch.sum(loss)
+    #     else:
+    #         raise NotImplementedError
+    #     return loss
 
     # def forward(self, preds, weight, bbox, eps=1e-7, reduction='mean'):
     #     """
