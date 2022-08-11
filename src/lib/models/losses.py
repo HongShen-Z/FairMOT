a3c0fd5ee8a160740615ba3b219c248ccca31941
ZHS <com.zhs@gmail.com>
2022-08-11 19:37:17 +0800

    wh reg change to giouloss

--- src/lib/models/losses.py
+++ src/lib/models/losses.py
@@ -145,38 +145,22 @@ class GiouLoss(nn.Module):
         print(preds)
         print('-' * 100)
         print(bbox)
-        ix1 = torch.max(preds[:, 0], bbox[:, 0])
-        iy1 = torch.max(preds[:, 1], bbox[:, 1])
-        ix2 = torch.min(preds[:, 2], bbox[:, 2])
-        iy2 = torch.min(preds[:, 3], bbox[:, 3])
+        x1 = torch.max(preds[:, 0], bbox[:, 0])
+        y1 = torch.max(preds[:, 1], bbox[:, 1])
+        x2 = torch.min(preds[:, 2], bbox[:, 2])
+        y2 = torch.min(preds[:, 3], bbox[:, 3])
 
-        iw = (ix2 - ix1 + 1.0).clamp(0.)
-        ih = (iy2 - iy1 + 1.0).clamp(0.)
+        w = (x2 - x1 + 1.0).clamp(0.)
+        h = (y2 - y1 + 1.0).clamp(0.)
 
-        # overlap
-        inters = iw * ih
+        inters = w * h
 
-        # union
         uni = (preds[:, 2] - preds[:, 0] + 1.0) * (preds[:, 3] - preds[:, 1] + 1.0) + (
                     bbox[:, 2] - bbox[:, 0] + 1.0) * (
-                      bbox[:, 3] - bbox[:, 1] + 1.0) - inters + eps
+                      bbox[:, 3] - bbox[:, 1] + 1.0) - inters
 
-        # ious
-        ious = inters / uni
-
-        ex1 = torch.min(preds[:, 0], bbox[:, 0])
-        ey1 = torch.min(preds[:, 1], bbox[:, 1])
-        ex2 = torch.max(preds[:, 2], bbox[:, 2])
-        ey2 = torch.max(preds[:, 3], bbox[:, 3])
-        ew = (ex2 - ex1 + 1.0).clamp(min=0.)
-        eh = (ey2 - ey1 + 1.0).clamp(min=0.)
-
-        # enclose erea
-        enclose = ew * eh + eps
-
-        giou = ious - (enclose - uni) / enclose
-
-        loss = 1 - giou
+        ious = (inters / uni).clamp(min=eps)
+        loss = -ious.log()
 
         if reduction == 'mean':
             loss = torch.mean(loss)
@@ -185,6 +169,62 @@ class GiouLoss(nn.Module):
         else:
             raise NotImplementedError
         return loss
+
+    # def forward(self, preds, weight, bbox, eps=1e-7, reduction='mean'):
+    #     """
+    #    https://github.com/sfzhang15/ATSS/blob/master/atss_core/modeling/rpn/atss/loss.py#L36
+    #     :param preds:[[x1,y1,x2,y2], [x1,y1,x2,y2],,,]
+    #     :param bbox:[[x1,y1,x2,y2], [x1,y1,x2,y2],,,]
+    #     :return: loss
+    #     """
+    #     pos_mask = weight > 1e-3
+    #     # avg_factor = torch.sum(pos_mask).float().item() + 1e-4
+    #     preds = preds[pos_mask].view(-1, 4)
+    #     bbox = bbox[pos_mask].view(-1, 4)
+    #     print('#' * 100)
+    #     print(preds)
+    #     print('-' * 100)
+    #     print(bbox)
+    #     ix1 = torch.max(preds[:, 0], bbox[:, 0])
+    #     iy1 = torch.max(preds[:, 1], bbox[:, 1])
+    #     ix2 = torch.min(preds[:, 2], bbox[:, 2])
+    #     iy2 = torch.min(preds[:, 3], bbox[:, 3])
+    #
+    #     iw = (ix2 - ix1 + 1.0).clamp(0.)
+    #     ih = (iy2 - iy1 + 1.0).clamp(0.)
+    #
+    #     # overlap
+    #     inters = iw * ih
+    #
+    #     # union
+    #     uni = (preds[:, 2] - preds[:, 0] + 1.0) * (preds[:, 3] - preds[:, 1] + 1.0) + (
+    #                 bbox[:, 2] - bbox[:, 0] + 1.0) * (
+    #                   bbox[:, 3] - bbox[:, 1] + 1.0) - inters + eps
+    #
+    #     # ious
+    #     ious = inters / uni
+    #
+    #     ex1 = torch.min(preds[:, 0], bbox[:, 0])
+    #     ey1 = torch.min(preds[:, 1], bbox[:, 1])
+    #     ex2 = torch.max(preds[:, 2], bbox[:, 2])
+    #     ey2 = torch.max(preds[:, 3], bbox[:, 3])
+    #     ew = (ex2 - ex1 + 1.0).clamp(min=0.)
+    #     eh = (ey2 - ey1 + 1.0).clamp(min=0.)
+    #
+    #     # enclose erea
+    #     enclose = ew * eh + eps
+    #
+    #     giou = ious - (enclose - uni) / enclose
+    #
+    #     loss = 1 - giou
+    #
+    #     if reduction == 'mean':
+    #         loss = torch.mean(loss)
+    #     elif reduction == 'sum':
+    #         loss = torch.sum(loss)
+    #     else:
+    #         raise NotImplementedError
+    #     return loss
 
 
     # def forward(self, pred, weight, target):
