5356426e2891976d3a877cf82ad6165c4e204ac5
ZHS <com.zhs@gmail.com>
2022-08-19 18:20:53 +0800

    wh reg change to diouloss

--- src/lib/tracking_utils/kalman_filter.py
+++ src/lib/tracking_utils/kalman_filter.py
@@ -72,14 +72,23 @@ class KalmanFilter(object):
         mean_vel = np.zeros_like(mean_pos)
         mean = np.r_[mean_pos, mean_vel]
 
+        # std = [
+        #     2 * self._std_weight_position * measurement[3],
+        #     2 * self._std_weight_position * measurement[3],
+        #     1e-2,
+        #     2 * self._std_weight_position * measurement[3],
+        #     10 * self._std_weight_velocity * measurement[3],
+        #     10 * self._std_weight_velocity * measurement[3],
+        #     1e-5,
+        #     10 * self._std_weight_velocity * measurement[3]]
         std = [
-            2 * self._std_weight_position * measurement[3],
-            2 * self._std_weight_position * measurement[3],
-            1e-2,
-            2 * self._std_weight_position * measurement[3],
-            10 * self._std_weight_velocity * measurement[3],
-            10 * self._std_weight_velocity * measurement[3],
-            1e-5,
+            2 * self._std_weight_position * measurement[0],  # the center point x
+            2 * self._std_weight_position * measurement[1],  # the center point y
+            1 * measurement[2],  # the ratio of width/height
+            2 * self._std_weight_position * measurement[3],  # the height
+            10 * self._std_weight_velocity * measurement[0],
+            10 * self._std_weight_velocity * measurement[1],
+            0.1 * measurement[2],
             10 * self._std_weight_velocity * measurement[3]]
         covariance = np.diag(np.square(std))
         return mean, covariance
