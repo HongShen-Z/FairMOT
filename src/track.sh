f628d263406a5354dab6102d1f5cc652a84bea11
ZHS <com.zhs@gmail.com>
2022-08-19 18:03:36 +0800

    wh reg change to diouloss

--- src/track.sh
+++ src/track.sh
@@ -9,10 +9,10 @@ module load anaconda3
 source activate
 conda deactivate
 conda activate fair
-python track.py mot --val_mot20 True --load_model ../exp/mot/ch_dla34_wh4/models/model_last.pth --conf_thres 0.3 \
---gpus $CUDA_VISIBLE_DEVICES --exp_id MOT20_ch_wh4
-python track.py mot --val_mot15 True --load_model ../exp/mot/ch_dla34_wh4/models/model_last.pth --conf_thres 0.6 \
---gpus $CUDA_VISIBLE_DEVICES --exp_id MOT15_ch_wh4
+python track.py mot --val_mot20 True --load_model ../exp/mot/ch_dla34_wh5/models/model_last.pth --conf_thres 0.3 \
+--gpus $CUDA_VISIBLE_DEVICES --exp_id MOT20_ch_wh5
+python track.py mot --val_mot15 True --load_model ../exp/mot/ch_dla34_wh5/models/model_last.pth --conf_thres 0.6 \
+--gpus $CUDA_VISIBLE_DEVICES --exp_id MOT15_ch_wh5
 #python track.py mot --exp_id MOT15val_dla34 --load_model ../models/fairmot_dla34.pth \
 #--conf_thres 0.6 --gpus $CUDA_VISIBLE_DEVICES
 
