b29ee4d5785e3c761f40b5056304f2f531a60df1
ZHS <com.zhs@gmail.com>
2022-08-20 23:20:47 +0800

    wh reg change to eiou loss

--- src/lib/models/losses.py
+++ src/lib/models/losses.py
@@ -199,8 +199,8 @@ class GiouLoss(nn.Module):
 
         # Focal-EIOU
         eiou = torch.mean((1 - iou + diou_term + eiou_term) * iou_weight)
-        focal_eiou = iou ** 0.5 * eiou
-        return focal_eiou
+        # focal_eiou = iou ** 0.5 * eiou
+        return eiou
 
     # def forward(self, preds, weight, bbox, eps=1e-7, reduction='mean'):
     #     """
