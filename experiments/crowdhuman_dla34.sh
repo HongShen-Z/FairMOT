0ef288e27a412f8367a8fb72b206b3e0a26429e8
ZHS <com.zhs@gmail.com>
2022-09-02 16:54:52 +0800

    wh reg change to eiou loss/resnet(0)

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -15,6 +15,6 @@ cd src
 #--lr_step '800' --lr 1e-4 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
 python train.py mot --exp_id ch_res34_whe --gpus $CUDA_VISIBLE_DEVICES --arch 'resdcn_34' \
 --batch_size 32 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 100 \
---lr_step '800' --lr 1e-2 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
+--lr_step '800' --lr 1e-4 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
 cd ..
 #--load_model '../models/ctdet_coco_dla_2x.pth' '../exp/mot/ch_dla34_wh_eiou3/models/model_210.pth'
