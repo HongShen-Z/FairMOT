5440fb162768ec706a0ce72ee827384a3dee9752
ZHS <com.zhs@gmail.com>
2022-09-02 17:39:50 +0800

    wh reg change to eiou loss/resnet(0)

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -10,11 +10,11 @@ source activate
 conda deactivate
 conda activate fair
 cd src
-python train.py mot --exp_id ch_dla34_wh_eiou4 --gpus $CUDA_VISIBLE_DEVICES --resume \
---batch_size 32 --load_model '../exp/mot/ch_dla34_wh_eiou5/models/model_265.pth' --num_epochs 400 \
---lr_step '800' --lr 3e-6 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
-#python train.py mot --exp_id ch_res34_whe --gpus $CUDA_VISIBLE_DEVICES --arch 'resdcn_34' \
-#--batch_size 32 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 100 \
-#--lr_step '800' --lr 1e-4 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
+#python train.py mot --exp_id ch_dla34_wh_eiou4 --gpus $CUDA_VISIBLE_DEVICES --resume \
+#--batch_size 32 --load_model '../exp/mot/ch_dla34_wh_eiou5/models/model_265.pth' --num_epochs 400 \
+#--lr_step '800' --lr 3e-6 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
+python train.py mot --exp_id ch_res34_whe --gpus $CUDA_VISIBLE_DEVICES --arch 'resdcn_34' \
+--batch_size 32 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 100 \
+--lr_step '800' --lr 1e-4 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
 cd ..
 #--load_model '../models/ctdet_coco_dla_2x.pth' '../exp/mot/ch_dla34_wh_eiou3/models/model_210.pth'
