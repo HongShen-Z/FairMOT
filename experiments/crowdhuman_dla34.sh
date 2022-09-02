256c0bd892981aa1bf365b1c126e47560358eca6
ZHS <com.zhs@gmail.com>
2022-09-02 11:00:23 +0800

    wh reg change to eiou loss(010)

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -15,6 +15,6 @@ cd src
 #--lr_step '800' --lr 1e-4 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
 python train.py mot --exp_id ch_dla34_whe1 --gpus $CUDA_VISIBLE_DEVICES --resume \
 --batch_size 32 --load_model '../exp/mot/ch_dla34_whe/models/model_10.pth' --num_epochs 200 \
---lr_step '800' --lr 5e-4 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
+--lr_step '800' --lr 1e-3 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
 cd ..
 #--load_model '../models/ctdet_coco_dla_2x.pth' '../exp/mot/ch_dla34_wh_eiou3/models/model_210.pth'
