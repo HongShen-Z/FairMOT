c8f964054dfdcd12a1850321432802c5697e0eec
ZHS <com.zhs@gmail.com>
2022-09-12 11:27:42 +0800

    mgda-parallel

--- src/lib/trains/_base_trainer.py
+++ src/lib/trains/_base_trainer.py
@@ -121,7 +121,6 @@ class BaseTrainer(object):
             self.model_with_loss = DataParallel(
                 self.model_with_loss, device_ids=gpus,
                 chunk_sizes=chunk_sizes).to(device)
-            print(self.model_with_loss, '1')
         else:
             self.model_with_loss = self.model_with_loss.to(device)
 
