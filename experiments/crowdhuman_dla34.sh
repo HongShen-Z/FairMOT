dc52b98bd86b53b91654fefedd6d1a4df2c5878d
ZHS <com.zhs@gmail.com>
2022-09-21 11:20:49 +0800

    add gradnorm

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -3,7 +3,7 @@
 #BSUB -o %J.out
 #BSUB -e %J.err
 #BSUB -q gpu_v100
-#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+#BSUB -gpu "num=2:mode=exclusive_process:aff=yes"
 
 module load anaconda3
 source activate
@@ -12,7 +12,7 @@ conda activate fair
 cd src
 python train.py mot --exp_id ch_dla34_grad --gpus $CUDA_VISIBLE_DEVICES \
 --batch_size 32 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 100 \
---lr_step '200' --lr 1e-3 --data_cfg '../src/lib/cfg/crowdhuman.json'
+--lr_step '200' --lr 1e-4 --data_cfg '../src/lib/cfg/crowdhuman.json'
 #python train.py mot --exp_id ch_res34_whe2 --gpus $CUDA_VISIBLE_DEVICES --arch 'resdcn_34' --resume \
 #--batch_size 32 --num_epochs 300 --load_model '../exp/mot/ch_res34_whe1/models/model_last.pth' \
 #--lr_step '800' --lr 1e-5 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
