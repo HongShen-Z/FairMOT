18b4bd1810a44a9d17b79703217a789ba9b6ccb9
ZHS <com.zhs@gmail.com>
2022-09-22 16:15:15 +0800

    gradnorm-wh(iou)

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -10,9 +10,9 @@ source activate
 conda deactivate
 conda activate fair
 cd src
-python train.py mot --exp_id ch_dla34_gd --gpus $CUDA_VISIBLE_DEVICES \
+python train.py mot --exp_id ch_dla34_gd_wh --gpus $CUDA_VISIBLE_DEVICES \
 --batch_size 32 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 100 \
---lr_step '200' --lr 1e-4 --data_cfg '../src/lib/cfg/crowdhuman.json'
+--lr_step '50' --lr 2e-4 --data_cfg '../src/lib/cfg/crowdhuman.json'
 #python train.py mot --exp_id ch_res34_whe2 --gpus $CUDA_VISIBLE_DEVICES --arch 'resdcn_34' --resume \
 #--batch_size 32 --num_epochs 300 --load_model '../exp/mot/ch_res34_whe1/models/model_last.pth' \
 #--lr_step '800' --lr 1e-5 --data_cfg '../src/lib/cfg/crowdhuman.json' --dense_wh --wh_weight 5.0
