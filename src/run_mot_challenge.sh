d23b2109c98a3d1e7b26d5bc09b6f6d08f6e30af
ZHS <com.zhs@gmail.com>
2022-09-25 17:29:43 +0800

    gradnorm-alpha=0.5-test/change to official testtool TrackEval

--- src/run_mot_challenge.sh
+++ src/run_mot_challenge.sh
@@ -10,6 +10,6 @@ source activate
 conda deactivate
 conda activate fair
 python TrackEval/scripts/run_mot_challenge.py --TRACKERS_TO_EVAL 'MOT20_ch_gd0.5' --BENCHMARK 'MOT20' \
---METRICS 'HOTA' 'CLEAR' 'Identity' --SKIP_SPLIT_FOL True --USE_PARALLEL True --NUM_PARALLEL_CORES 2\
+--METRICS 'HOTA' 'CLEAR' 'Identity' --SKIP_SPLIT_FOL True --USE_PARALLEL True --NUM_PARALLEL_CORES 2 \
 --TRACKERS_FOLDER '/seu_share/home/dijunyong/220205723/projects/FairMOT/demos' \
 --GT_FOLDER '/seu_share/home/dijunyong/220205723/datasets/MOT/data/gt/mot_challenge/'
