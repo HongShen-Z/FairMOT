8079985af3f20294bf62c401df56e6bbbe91386e
ZHS <com.zhs@gmail.com>
2022-09-27 21:56:04 +0800

    val baseline model on half MOT17 train set(val set)

---
+++ src/mot_val_utils.py
@@ -0,0 +1,12 @@
+import os
+import os.path as osp
+import glob
+import shutil
+
+
+path = osp.join(os.getcwd(), '../demos')
+for file in glob.glob(path + '/*SDP.txt'):
+    new = osp.basename(file).split('SDP')[0]
+    shutil.copy(file, osp.join(path, new + 'DPM.txt'))
+    shutil.copy(file, osp.join(path, new + 'FRCNN.txt'))
+
--- src/track.sh
+++ src/track.sh
@@ -9,8 +9,9 @@ module load anaconda3
 source activate
 conda deactivate
 conda activate fair
-python track_half.py mot --val_mot17 True --load_model ../exp/mix_mot17_half_base/models/model_last.pth \
---conf_thres 0.4 --gpus $CUDA_VISIBLE_DEVICES --exp_id MOT17_ablation_base
+#python track_half.py mot --val_mot17 True --load_model ../exp/mix_mot17_half_base/models/model_last.pth \
+#--conf_thres 0.4 --gpus $CUDA_VISIBLE_DEVICES --exp_id MOT17_ablation_base
+python mot_val_utils.py
 python TrackEval/scripts/run_mot_challenge.py --TRACKERS_TO_EVAL 'MOT17_ablation_base' --BENCHMARK 'MOT17' \
 --METRICS 'HOTA' 'CLEAR' 'Identity' --SKIP_SPLIT_FOL True --USE_PARALLEL True --NUM_PARALLEL_CORES 2 \
 --TRACKERS_FOLDER '/seu_share/home/dijunyong/220205723/projects/FairMOT/demos' \
