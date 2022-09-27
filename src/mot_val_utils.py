95b81e616119f9cd11b7ce6cd318f247979d5bcc
ZHS <com.zhs@gmail.com>
2022-09-27 21:56:50 +0800

    val baseline model on half MOT17 train set(val set)

--- src/mot_val_utils.py
+++ src/mot_val_utils.py
@@ -4,7 +4,7 @@ import glob
 import shutil
 
 
-path = osp.join(os.getcwd(), '../demos')
+path = osp.join(os.getcwd(), '../demos/MOT17_ablation_base/data')
 for file in glob.glob(path + '/*SDP.txt'):
     new = osp.basename(file).split('SDP')[0]
     shutil.copy(file, osp.join(path, new + 'DPM.txt'))
