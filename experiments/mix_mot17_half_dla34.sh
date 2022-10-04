e98c3e0d388ece79d0cc0ff7992fe68f92ea749b
ZHS <com.zhs@gmail.com>
2022-10-04 14:36:28 +0800

    debug wh(0.5)

--- experiments/mix_mot17_half_dla34.sh
+++ experiments/mix_mot17_half_dla34.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BSUB -J train_eiou
+#BSUB -J eiou0.5
 #BSUB -o %J.out
 #BSUB -e %J.err
 #BSUB -q gpu_v100
@@ -10,8 +10,8 @@ source activate
 conda deactivate
 conda activate fair
 cd src
-python train.py mot --exp_id ablation_oval_eiou_grad --num_epochs 30 --lr_step '20' --multi_loss 'gradnorm' \
+python train.py mot --exp_id ablation_oval_eiou05_grad --num_epochs 30 --lr_step '20' --multi_loss 'gradnorm' \
 --gpus $CUDA_VISIBLE_DEVICES --load_model '../models/ctdet_coco_dla_2x.pth' --hm_shape 'oval' \
---dense_wh --wh_weight 0.1 \
+--dense_wh --wh_weight 0.5 \
 --data_cfg '../src/lib/cfg/data_half.json' --batch_size 48 --lr 2e-4
 cd ..
