d8c3809d65fe30a445a9c95a4878df3197a22497
ZHS <com.zhs@gmail.com>
2022-10-20 20:13:23 +0800

    test wh

--- experiments/mix_mot17_half_dla34.sh
+++ experiments/mix_mot17_half_dla34.sh
@@ -3,7 +3,7 @@
 #BSUB -o %J.out
 #BSUB -e %J.err
 #BSUB -q gpu_v100
-#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+#BSUB -gpu "num=2:mode=exclusive_process:aff=yes"
 
 module load anaconda3
 source activate
@@ -12,6 +12,6 @@ conda activate fair
 cd src
 python train.py mot --exp_id ablation_wh1 --num_epochs 30 --lr_step '20' --multi_loss 'gradnorm' \
 --gpus $CUDA_VISIBLE_DEVICES --load_model '../models/ctdet_coco_dla_2x.pth' --hm_shape 'oval' \
---dense_wh \
---data_cfg '../src/lib/cfg/data_half.json' --batch_size 24 --lr 2e-4
+--dense_wh --wh_weight 0.5 \
+--data_cfg '../src/lib/cfg/data_half.json' --batch_size 48 --lr 2e-4
 cd ..
