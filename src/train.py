de0964744665a0776cdd8056dbb644452071936d
ZHS <com.zhs@gmail.com>
2022-10-26 23:05:37 +0800

    test reidup

--- src/train.py
+++ src/train.py
@@ -106,4 +106,4 @@ if __name__ == '__main__':
              'id': 128,
              'reg': 2}
     model = create_model('dla_34', heads, 256)
-    print(model.dla_up.ida_2.parameters())
+    print(model.dla_up.ida_2)
