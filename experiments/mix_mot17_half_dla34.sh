901819e951537e9241037eed50f318c70ac73cde
ZHS <com.zhs@gmail.com>
2022-11-04 21:22:53 +0800

    dla + mti-net

--- experiments/mix_mot17_half_dla34.sh
+++ experiments/mix_mot17_half_dla34.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BSUB -J oval_uniform
+#BSUB -J arch
 #BSUB -o %J.out
 #BSUB -e %J.err
 #BSUB -q gpu_v100
@@ -10,7 +10,7 @@ source activate
 conda deactivate
 conda activate fair
 cd src
-python train.py mot --exp_id abl_fo --num_epochs 30 --lr_step '20' --multi_loss 'fix' \
+python train.py mot --exp_id abl_go_arch --num_epochs 30 --lr_step '20' --multi_loss 'gradnorm' \
 --gpus $CUDA_VISIBLE_DEVICES --load_model '../models/ctdet_coco_dla_2x.pth' --hm_shape 'oval' \
 --data_cfg '../src/lib/cfg/data_half.json' --batch_size 32 --lr 2e-4
 cd ..
