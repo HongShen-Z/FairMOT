f67dee5e3236a0d6c8f8cae4d151fee5873cfe71
ZHS <com.zhs@gmail.com>
2022-11-13 23:43:05 +0800

    dla + ITP + MMD(abl_go_arch-dev2)

--- experiments/mix_mot17_half_dla34.sh
+++ experiments/mix_mot17_half_dla34.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BSUB -J arch-dev
+#BSUB -J arch-dev2
 #BSUB -o %J.out
 #BSUB -e %J.err
 #BSUB -q gpu_v100
@@ -10,7 +10,7 @@ source activate
 conda deactivate
 conda activate fair
 cd src
-python train.py mot --exp_id abl_go_arch-dev --num_epochs 30 --lr_step '20' --multi_loss 'gradnorm' \
+python train.py mot --exp_id abl_go_arch-dev2 --num_epochs 30 --lr_step '20' --multi_loss 'gradnorm' \
 --gpus $CUDA_VISIBLE_DEVICES --load_model '../models/ctdet_coco_dla_2x.pth' --hm_shape 'oval' \
 --data_cfg '../src/lib/cfg/data_half.json' --batch_size 24 --lr 2e-4
 cd ..
