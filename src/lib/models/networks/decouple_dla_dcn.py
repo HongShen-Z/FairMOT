14981ec5dcb87b25e37ce1294676e75847291347
ZHS <com.zhs@gmail.com>
2022-11-20 20:46:59 +0800

    abl_fo_mti

--- src/lib/models/networks/decouple_dla_dcn.py
+++ src/lib/models/networks/decouple_dla_dcn.py
@@ -373,14 +373,16 @@ def fill_fc_weights(layers):
 def weight_init(m):
     if isinstance(m, nn.Linear):
         nn.init.xavier_normal_(m.weight)
-        nn.init.constant_(m.bias, 0)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
     # 判断是否为conv2d，使用相应的初始化方式
     elif isinstance(m, nn.Conv2d):
         nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
     # 是否为批归一化层
     elif isinstance(m, nn.BatchNorm2d):
         nn.init.constant_(m.weight, 1)
-        nn.init.constant_(m.bias, 0)
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
 
 
 def fill_up_weights(up):
