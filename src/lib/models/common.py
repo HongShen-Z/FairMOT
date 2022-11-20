9211f66264cb4063f43bc0c92e93da8f5d7681fa
ZHS <com.zhs@gmail.com>
2022-11-20 22:06:02 +0800

    abl_fo_mti

--- src/lib/models/common.py
+++ src/lib/models/common.py
@@ -29,7 +29,7 @@ class Conv(nn.Module):
     # Standard convolution
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
         super(Conv, self).__init__()
-        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
+        self.conv = nn.Conv2d(c1, c2, k, s, k//2, groups=g, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         # self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
         self.act = nn.LeakyReLU(0.1, inplace=True) if act else nn.Identity()
