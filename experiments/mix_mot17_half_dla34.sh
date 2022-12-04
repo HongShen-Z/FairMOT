21d407c091dd3d91b3080a8682fb5680264c44d8
ZHS <com.zhs@gmail.com>
2022-12-04 21:13:03 +0800

    abl_fo_arch-dev1

--- experiments/mix_mot17_half_dla34.sh
+++ experiments/mix_mot17_half_dla34.sh
@@ -3,7 +3,7 @@
 #BSUB -o %J.out
 #BSUB -e %J.err
 #BSUB -q gpu_v100
-#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+#BSUB -gpu "num=2:mode=exclusive_process:aff=yes"
 
 module load anaconda3
 source activate
@@ -12,5 +12,5 @@ conda activate fair
 cd src
 python train.py mot --exp_id abl_fo_arch-dev1 --num_epochs 30 --lr_step '20' --multi_loss 'fix' \
 --gpus $CUDA_VISIBLE_DEVICES --load_model '../models/ctdet_coco_dla_2x.pth' --hm_shape 'oval' \
---data_cfg '../src/lib/cfg/data_half.json' --batch_size 12 --lr 2e-4
+--data_cfg '../src/lib/cfg/data_half.json' --batch_size 24 --lr 2e-4
 cd ..
