54a9fd84f9bb605a13e4c272bc6d4a9afad8c635
ZHS <com.zhs@gmail.com>
2023-03-18 16:22:15 +0800

    origin fairmot exp(fix_circle)

--- experiments/mix_mot17_half_dla34.sh
+++ experiments/mix_mot17_half_dla34.sh
@@ -3,7 +3,7 @@
 #BSUB -o %J.out
 #BSUB -e %J.err
 #BSUB -q gpu_v100
-#BSUB -gpu "num=2:mode=exclusive_process:aff=yes"
+#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
 
 module load anaconda3
 source activate
@@ -12,6 +12,6 @@ conda activate fair
 cd src
 python train.py mot --exp_id fix_circle --num_epochs 30 --lr_step '20' --multi_loss 'fix' \
 --gpus $CUDA_VISIBLE_DEVICES --load_model '../models/ctdet_coco_dla_2x.pth' --hm_shape 'circle' \
---data_cfg '../src/lib/cfg/data_half.json' --batch_size 24 --lr 2e-4
+--data_cfg '../src/lib/cfg/data_half.json' --batch_size 12 --lr 1e-4
 cd ..
 # --lr 2e-4
