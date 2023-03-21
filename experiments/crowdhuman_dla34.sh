2fe65425bb36b248036a38566e88de249173e02e
ZHS <com.zhs@gmail.com>
2023-03-21 16:22:58 +0800

    abl: oval_0.55

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -10,7 +10,7 @@ source activate
 conda deactivate
 conda activate fair
 cd src
-python train.py mot --exp_id ch_0.4 --gpus $CUDA_VISIBLE_DEVICES --multi_loss 'uncertainty' --alpha 0.4 \
+python train.py mot --exp_id ch_0.55 --gpus $CUDA_VISIBLE_DEVICES --multi_loss 'uncertainty' --alpha 0.55 \
 --batch_size 16 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 30 --hm_shape 'oval' \
 --lr_step '20' --lr 2e-4 --data_cfg '../src/lib/cfg/crowdhuman.json' --output-root '../demos/abl/oval'
 #python train.py mot --exp_id ch_res34_whe2 --gpus $CUDA_VISIBLE_DEVICES --arch 'resdcn_34' --resume \
