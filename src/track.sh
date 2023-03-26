2c6ccbce696a0a94b95a043af0cd5056cd9248b3
ZHS <com.zhs@gmail.com>
2023-03-26 23:52:23 +0800

    abl-track: ITP

--- src/track.sh
+++ src/track.sh
@@ -9,9 +9,9 @@ module load anaconda3
 source activate
 conda deactivate
 conda activate fair
-python track_half.py mot --val_mot17 True --load_model ../exp/net/ch_MMD/models/model_last.pth \
---conf_thres 0.4 --gpus $CUDA_VISIBLE_DEVICES --exp_id MOT17_ch_MMD --output-root '../demos/abl/net'
-python TrackEval/scripts/run_mot_challenge.py --TRACKERS_TO_EVAL 'MOT17_ch_MMD' --BENCHMARK 'MOT17' \
+python track_half.py mot --val_mot17 True --load_model ../exp/net/ch_ITP/models/model_last.pth \
+--conf_thres 0.4 --gpus $CUDA_VISIBLE_DEVICES --exp_id MOT17_ch_ITP --output-root '../demos/abl/net'
+python TrackEval/scripts/run_mot_challenge.py --TRACKERS_TO_EVAL 'MOT17_ch_ITP' --BENCHMARK 'MOT17' \
 --METRICS 'HOTA' 'CLEAR' 'Identity' --SKIP_SPLIT_FOL True --USE_PARALLEL True --NUM_PARALLEL_CORES 2 \
 --GT_LOC_FORMAT '{gt_folder}/{seq}/gt/gt_val_half.txt' \
 --TRACKERS_FOLDER '/seu_share/home/dijunyong/220205723/projects/FairMOT/demos/abl/net' \
