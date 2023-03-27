74e46e83e9fd107b524c3ffc1871def7966d7c50
ZHS <com.zhs@gmail.com>
2023-03-27 16:08:20 +0800

    abl-net: ITP-MMD

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -11,7 +11,7 @@ conda deactivate
 conda activate fair
 cd src
 python train.py mot --exp_id ch_ITP-MMD --gpus $CUDA_VISIBLE_DEVICES --multi_loss 'uncertainty' --alpha 0.25 \
---batch_size 12 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 30 --hm_shape 'oval' \
+--batch_size 10 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 30 --hm_shape 'oval' \
 --lr_step '20' --lr 2e-4 --data_cfg '../src/lib/cfg/crowdhuman.json' --output-root '../demos/abl/net'
 #python train.py mot --exp_id ch_res34_whe2 --gpus $CUDA_VISIBLE_DEVICES --arch 'resdcn_34' --resume \
 #--batch_size 32 --num_epochs 300 --load_model '../exp/mot/ch_res34_whe1/models/model_last.pth' \
