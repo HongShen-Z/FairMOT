5cb8e875f6e8844289d843f90dd29a481a04acbe
ZHS <com.zhs@gmail.com>
2023-03-31 13:40:41 +0800

    yolo car

--- src/lib/trains/mot.py
+++ src/lib/trains/mot.py
@@ -89,28 +89,27 @@ class MotLoss(torch.nn.Module):
             off_loss += self.crit_reg(outputs['reg'], batch['reg_mask'],
                                       batch['ind'], batch['reg'])
 
-        if opt.id_weight > 0:
-            # id_head_scale = _tranpose_and_gather_feat(pred_scale['id'], batch['ind'])
-            # id_head_scale = id_head_scale[batch['reg_mask'] > 0].contiguous()
-            # id_head_scale = self.emb_scale * F.normalize(id_head_scale)
+        # id_head_scale = _tranpose_and_gather_feat(pred_scale['id'], batch['ind'])
+        # id_head_scale = id_head_scale[batch['reg_mask'] > 0].contiguous()
+        # id_head_scale = self.emb_scale * F.normalize(id_head_scale)
 
-            id_head = _tranpose_and_gather_feat(outputs['id'], batch['ind'])
-            id_head = id_head[batch['reg_mask'] > 0].contiguous()
-            id_head = self.emb_scale * F.normalize(id_head)
-            id_target = batch['ids'][batch['reg_mask'] > 0]
+        id_head = _tranpose_and_gather_feat(outputs['id'], batch['ind'])
+        id_head = id_head[batch['reg_mask'] > 0].contiguous()
+        id_head = self.emb_scale * F.normalize(id_head)
+        id_target = batch['ids'][batch['reg_mask'] > 0]
 
-            # id_output_scale = self.classifier(id_head_scale).contiguous()
-            id_output = self.classifier(id_head).contiguous()
-            if self.opt.id_loss == 'focal':
-                id_target_one_hot = id_output.new_zeros((id_head.size(0), self.nID)).scatter_(1,
-                                                                                              id_target.long().view(
-                                                                                                  -1, 1), 1)
-                id_loss += sigmoid_focal_loss_jit(id_output, id_target_one_hot,
-                                                  alpha=0.25, gamma=2.0, reduction="sum"
-                                                  ) / id_output.size(0)
-            else:
-                # id_loss += self.IDLoss(id_output_scale, id_target)
-                id_loss += self.IDLoss(id_output, id_target)
+        # id_output_scale = self.classifier(id_head_scale).contiguous()
+        id_output = self.classifier(id_head).contiguous()
+        if self.opt.id_loss == 'focal':
+            id_target_one_hot = id_output.new_zeros((id_head.size(0), self.nID)).scatter_(1,
+                                                                                          id_target.long().view(
+                                                                                              -1, 1), 1)
+            id_loss += sigmoid_focal_loss_jit(id_output, id_target_one_hot,
+                                              alpha=0.25, gamma=2.0, reduction="sum"
+                                              ) / id_output.size(0)
+        else:
+            # id_loss += self.IDLoss(id_output_scale, id_target)
+            id_loss += self.IDLoss(id_output, id_target)
 
         det_loss = opt.hm_weight * hm_loss + opt.wh_weight * wh_loss + opt.off_weight * off_loss
 
