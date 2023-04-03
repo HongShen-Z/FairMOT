5c6a9fa32a419c83cc1fc7ce4e4470897731bad6
ZHS <com.zhs@gmail.com>
2023-04-03 14:27:13 +0800

    benchmark: ch + mix

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -3,7 +3,7 @@
 #BSUB -o %J.out
 #BSUB -e %J.err
 #BSUB -q gpu_v100
-#BSUB -gpu "num=1:mode=exclusive_process:aff=yes"
+#BSUB -gpu "num=2:mode=exclusive_process:aff=yes"
 
 module load anaconda3
 source activate
@@ -11,7 +11,7 @@ conda deactivate
 conda activate fair
 cd src
 python train.py mot --exp_id ch_fix_ITP-MMD --gpus $CUDA_VISIBLE_DEVICES --multi_loss 'fix' --alpha 0.25 \
---batch_size 12 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 30 --hm_shape 'oval' \
+--batch_size 16 --load_model '../models/ctdet_coco_dla_2x.pth' --num_epochs 30 --hm_shape 'oval' \
 --lr_step '20' --lr 1e-4 --data_cfg '../src/lib/cfg/crowdhuman.json' --output-root '../demos/abl'
 #python train.py mot --exp_id ch_res34_whe2 --gpus $CUDA_VISIBLE_DEVICES --arch 'resdcn_34' --resume \
 #--batch_size 32 --num_epochs 300 --load_model '../exp/mot/ch_res34_whe1/models/model_last.pth' \
