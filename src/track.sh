c97fac04ab24547bd9be4c3f816f953e0ff9096b
ZHS <com.zhs@gmail.com>
2023-04-06 17:53:44 +0800

    benchmark: ITP-MMD(test_mot16)

--- src/track.sh
+++ src/track.sh
@@ -9,8 +9,8 @@ module load anaconda3
 source activate
 conda deactivate
 conda activate fair
-python track.py mot --test_mot15 True --load_model ../exp/mot15_ft_mix_ITP-MMD/models/model_last.pth \
---conf_thres 0.3 --gpus $CUDA_VISIBLE_DEVICES --exp_id MOT15_giou_ITP-MMD --output-root '../demos/benchmark'
+python track.py mot --test_mot16 True --load_model ../exp/mix_ft_ch_ITP-MMD/models/model_last.pth \
+--conf_thres 0.4 --gpus $CUDA_VISIBLE_DEVICES --exp_id MOT16_giou_ITP-MMD --output-root '../demos/benchmark'
 #python TrackEval/scripts/run_mot_challenge.py --TRACKERS_TO_EVAL 'MOT17_giou_ITP-MMD' --BENCHMARK 'MOT17' \
 #--METRICS 'HOTA' 'CLEAR' 'Identity' --SKIP_SPLIT_FOL True --USE_PARALLEL True --NUM_PARALLEL_CORES 2 \
 #--GT_LOC_FORMAT '{gt_folder}/{seq}/gt/gt.txt' \
