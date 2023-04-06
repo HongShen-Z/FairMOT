2f82f9b17674391019adebbe7548daa26d48a61f
ZHS <com.zhs@gmail.com>
2023-04-06 21:28:44 +0800

    benchmark: ITP-MMD(test_mot20)

--- experiments/mix_ft_ch_dla34.sh
+++ experiments/mix_ft_ch_dla34.sh
@@ -10,7 +10,7 @@ source activate
 conda deactivate
 conda activate fair
 cd src
-python train.py mot --exp_id mix_ft_ch_ITP-MMD --load_model '../exp/ch_fix_ITP-MMD/models/model_last.pth' \
+python train.py mot --exp_id mix_ft_ch_ITP-MMD-MOT20 --load_model '../exp/ch_fix_ITP-MMD/models/model_last.pth' \
 --data_cfg '../src/lib/cfg/data.json' --gpus $CUDA_VISIBLE_DEVICES --multi_loss 'fix' --alpha 0.25 \
 --batch_size 24 --lr 2e-4 --hm_shape 'oval'
 cd ..
